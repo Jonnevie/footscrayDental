dcb26fd13b60141c4a6ac5a62fc88491bdc26cfe
Jonnevie <56157123+Jonnevie@users.noreply.github.com>
2022-08-04 11:36:03 +1000

    icons grid

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("JS ready");
+window.addEventListener("scroll", function () {
+  let nav = document.getElementById("nav");
+
+  nav.classList.toggle("scroll-active", window.scrollY > 0);
+});
